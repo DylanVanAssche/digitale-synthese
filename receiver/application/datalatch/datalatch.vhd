2b0580944505d36916a99e8f45ccac0244bcb4a8
Dylan Van Assche <dylan.van.assche@protonmail.com>
2017-12-09 11:57:07 +0100

    Fixed datalatch

--- receiver/application/datalatch/datalatch.vhd
+++ receiver/application/datalatch/datalatch.vhd
@@ -24,13 +24,13 @@ USE ieee.std_logic_unsigned.ALL;
 
 ENTITY datalatch IS
   PORT (
-		sh         : IN  std_logic;
 		clk        : IN  std_logic;
 		clk_en     : IN  std_logic;
 		rst        : IN  std_logic;
-		data       : IN std_logic;
-		value      : OUT std_logic_vector(3 DOWNTO 0); -- 4bits
-		preamble   : OUT std_logic_vector(6 DOWNTO 0) -- 7bits
+		bitsample  : IN  std_logic;
+		preamble   : IN  std_logic_vector(6 DOWNTO 0); -- 7bits
+		data_in    : IN  std_logic_vector(3 DOWNTO 0); -- 4bits
+		data_out   : OUT std_logic_vector(3 DOWNTO 0) 
     	 );
 END datalatch;
 
@@ -40,14 +40,14 @@ END datalatch;
 ARCHITECTURE behavior OF datalatch IS
 	SIGNAL latch: std_logic_vector(3 DOWNTO 0);
 	SIGNAL latch_next: std_logic_vector(3 DOWNTO 0);
-	CONSTANT PREAMBLE: std_logic_vector(6 DOWNTO 0) := "0111110";
+	CONSTANT PREAMBLE_VALUE: std_logic_vector(6 DOWNTO 0) := "0111110";
 BEGIN
 -- connect signal to output
-value <= latch;
+data_out <= latch;
 -- 2-Process: synchronous part
 latch_sync : PROCESS (clk)
 BEGIN
-	IF (rising_edge(clk) AND clk_en = '1') THEN
+	IF (rising_edge(clk) AND clk_en = '1' AND bitsample = '1') THEN
 		IF (rst = '1') THEN -- rst line high, go to initial state
 			latch <= (OTHERS => '0');
 		ELSE -- normal operation
@@ -56,12 +56,12 @@ BEGIN
 	END IF;
 END PROCESS latch_sync;
 -- 2-Process: combinatoric part
-latch_comb : PROCESS(preamble, value)
+latch_comb : PROCESS(preamble, data_in, latch)
 BEGIN
-  IF (preamble = PREAMBLE) THEN -- preamble found, set value
-    latch_next <= value;
-  ELSE -- keeping value until preamble is found
-    latch_next <= latch;
-  END IF;
+	IF (preamble = PREAMBLE_VALUE) THEN -- preamble found, set data in latch
+		latch_next <= data_in;
+	ELSE -- keeping value until preamble is found
+		latch_next <= latch;
+	END IF;
 END PROCESS latch_comb;
 END behavior;
